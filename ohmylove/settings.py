b1f5465bbffa722491d88d5f0c8aed0b11d6d417
André Lucas de Sousa Pinto <andrelucax@gmail.com>
2024-05-03 22:27:29 -0300

    Fixed env. Added migration

--- ohmylove/settings.py
+++ ohmylove/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-DEBUG = os.environ['DEBUG']
+DEBUG = os.environ['DEBUG'] == "true"
 
 ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
 
